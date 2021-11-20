9ee57891e52b60b4d2d6ba17ce22e856eba31b85
sla6ak <slabakxaker@gmail.com>
2021-11-21 00:31:07 +0200

    3 home-worked plus

--- js/task-01.js
+++ js/task-01.js
@@ -1,7 +1,7 @@
-const perentsLength = document.querySelector( '#categories' ).children.length;
+let perentsLength = document.querySelector( '#categories' ).children.length;
 console.log( 'Number of categories:', perentsLength )
 
-const titleContent = document.querySelectorAll( '.item' )
+let titleContent = document.querySelectorAll( '.item' )
     .forEach( ( elem, index, arr ) =>
         console.log('Category:', elem.firstElementChild.textContent,'Elements:', elem.lastElementChild.children.length ) );
 
